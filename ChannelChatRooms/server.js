bd3e165afbf93dcda033978ff8a6360b4b483eee
Shawn211 <csy291422638@gmail.com>
2018-08-21 11:39:22 +0800

    ChannelChatRooms:更新旧代码；同时修复信息逐个发送情况，更新逐行发送功能，同时识别shutdown

---
+++ ChannelChatRooms/server.js
@@ -0,0 +1,58 @@
+var events = require('events');
+var net = require('net');
+var channel = new events.EventEmitter();
+channel.clients = {};  // Id-client
+channel.subscriptions = {};  // Id-发布消息事件的执行函数(加入者，发布消息)
+var datas = '';
+
+channel.on('join', function(id, client){
+    this.clients[id] = client;  // 添加join事件的监听器，保存用户的client对象，以便程序可以将数据发送给用户
+    this.subscriptions[id] = function(senderId, message){
+        if(id != senderId){  // 忽略发出这一广播数据的用户
+            this.clients[id].write(message);
+        }
+    }
+    this.on('broadcast', this.subscriptions[id]);  // 添加一个专门针对当前用户的broadcast事件监听器
+});
+
+// 创建leave事件的监听器
+channel.on('leave', function(id){
+    channel.removeListener('broadcast', this.subscriptions[id]);
+    channel.emit('broadcast', id, id + ' has left the chat.\n');  // 移除指定客户端的broadcast监听器
+});
+
+channel.on('shutdown', function(){
+    channel.emit('broadcast', '', 'Chat has shutdown.\n');
+    channel.removeAllListeners('broadcast');
+});
+
+var server = net.createServer(function(client){
+    var id = client.remoteAddress + ':' + client.remotePort;
+    // net.createServer 的回调函数已为 "connection" 自动设置了一个监听器
+    channel.emit('join', id, client);  // 当有用户连到服务器上来时发出一个join事件，指明用户ID和client对象
+    // client.on('connect', function(){
+    //     channel.emit('join', id, client);
+    // });
+    client.on('data', function(data){
+        data = data.toString();
+        
+        if(data == '\r\n'){
+            datas += data;
+            if(datas == 'shutdown\r\n'){
+                channel.emit('shutdown');
+            }
+            channel.emit('broadcast', id, datas);  // 当有用户发送数据时，发出一个频道broadcast事件，指明用户ID和消息
+            datas = '';
+        }else{
+            datas += data;
+        }
+    });
+
+    // 在用户断开连接时发出leave事件
+    client.on('close', function(){
+        channel.emit('leave', id);
+    });
+});
+
+server.listen(8000);
+console.log('Server listening:8000')
