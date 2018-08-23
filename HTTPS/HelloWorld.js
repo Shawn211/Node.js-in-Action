38653b4b81d9b408d60ab2a6e8c5cb2765903df3
Shawn211 <csy291422638@gmail.com>
2018-08-23 11:55:12 +0800

    HTTPS

---
+++ HTTPS/HelloWorld.js
@@ -0,0 +1,15 @@
+var https = require('https');
+var fs = require('fs');
+
+var options = {
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./key-cert.pem')
+}
+
+var server = https.createServer(options, function(req, res){
+    res.writeHead(200);
+    res.end('<h1>Hello World!</h1>')
+});
+
+server.listen(8888);
+console.log('Server listening on port 8888');
